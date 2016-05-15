268e28074174028f10747da29633a9a202749c6a
Asraf <asraf.alom02@gmail.com>
2016-05-15 15:01:34 -0500

    implementing put method

--- src/datastructures/timetravelmap/MapTimeTravel.java
+++ src/datastructures/timetravelmap/MapTimeTravel.java
@@ -1,15 +1,26 @@
 package datastructures.timetravelmap;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class MapTimeTravel {
 	
-	class Node{
-		int k;
-		long t;
-		public Node(int k, long t) {
-			this.k = k;
-			this.t = t;
+	private Map<Integer, TreeMap<Long, Integer>> mapKtoTV = new HashMap<>();
+	
+	public void put(int k, long t, int v){
+		TreeMap<Long, Integer> mapTV;		
+		if(mapKtoTV.containsKey(k)){
+			mapTV = mapKtoTV.get(k);
+			mapTV.put(t, v);
 		}
+		else{
+			mapTV = new TreeMap<>();
+			mapTV.put(t, v);
+			mapKtoTV.put(k, mapTV);
+		}	
 	}
+	
 	
 	public static void main(String[] args) {
 		
