4ecc8ab3db87a467c5a74301fcf596a995aba4d1
Asraf <asraf.alom02@gmail.com>
2016-05-15 14:25:20 -0500

    added node class

--- src/datastructures/timetravelmap/MapTimeTravel.java
+++ src/datastructures/timetravelmap/MapTimeTravel.java
@@ -1,7 +1,15 @@
 package datastructures.timetravelmap;
 
 public class MapTimeTravel {
-
+	
+	class Node{
+		int k;
+		long t;
+		public Node(int k, long t) {
+			this.k = k;
+			this.t = t;
+		}
+	}
 	
 	public static void main(String[] args) {
 		
