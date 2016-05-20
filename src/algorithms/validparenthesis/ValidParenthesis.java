f38c4dba782197365dc42e8be8c388e77c50592c
Asraful Alom <asraf.alom02@gmail.com>
2016-05-19 23:05:13 -0500

    implemented the algorithm

--- src/algorithms/validparenthesis/ValidParenthesis.java
+++ src/algorithms/validparenthesis/ValidParenthesis.java
@@ -1,7 +1,27 @@
 package algorithms.validparenthesis;
 
-public class ValidParenthesis {
+import java.util.Stack;
 
+public class ValidParenthesis {
+	
+	public static boolean isValid(String s) {
+        if(s==null || s.isEmpty())	return false;
+		
+        Stack<Character> chStack = new Stack<Character>();
+		
+        for(char ch : s.toCharArray()){
+			if(ch=='(' || ch=='{' || ch=='[')
+				chStack.push(ch);
+			else if(ch==')' || ch=='}' || ch==']'){
+				if(chStack.isEmpty())	return false;
+				char pop = chStack.pop();
+				if(Math.abs(ch-pop) > 2)	return false;
+			}
+		}
+        
+		return chStack.isEmpty();
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
