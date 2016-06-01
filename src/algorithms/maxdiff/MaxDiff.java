207030f29ec2b2fbbad9222c6198437f2ae76c24
Asraf <asraf.alom02@gmail.com>
2016-06-01 00:33:37 -0500

    implemented algorithm

--- src/algorithms/maxdiff/MaxDiff.java
+++ src/algorithms/maxdiff/MaxDiff.java
@@ -1,9 +1,27 @@
 package algorithms.maxdiff;
 
 public class MaxDiff {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int findMaxDiff(int[] a){
+		if(a==null || a.length < 2)	return 0;
+		
+		int minPos=0, maxPos=-1;
+		int MIN = a[0], MAX=Integer.MIN_VALUE;
+		
+		for(int i=1; i<a.length; i++){
+			if(a[i] < MIN){
+				MIN = a[i];
+				minPos = i;				
+			}
+			else{
+				MAX = a[i];
+				maxPos = i;
+			}
+		}
+		
+		return (minPos < maxPos) ? (MAX - MIN) : 0;
+	}
+	
+	public static void main(String[] args) {	
 
 	}
 
