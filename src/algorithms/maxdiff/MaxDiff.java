778088414291b9ee256d708d435ee562a17f2de6
Asraf <asraf.alom02@gmail.com>
2016-06-01 22:33:47 -0500

    fixed algorithm to work in all cases

--- src/algorithms/maxdiff/MaxDiff.java
+++ src/algorithms/maxdiff/MaxDiff.java
@@ -1,25 +1,31 @@
 package algorithms.maxdiff;
 
-public class MaxDiff {
+public class MaxDiff {		
 	
 	public static int findMaxDiff(int[] a){
 		if(a==null || a.length < 2)	return 0;
 		
-		int minPos=0, maxPos=-1;
+		int minPos=0, tmpMinPos=0, maxPos=0;		
+		int maxDiff = 0;
 		int MIN = a[0], MAX=Integer.MIN_VALUE;
 		
 		for(int i=1; i<a.length; i++){
 			if(a[i] < MIN){
 				MIN = a[i];
-				minPos = i;				
+				tmpMinPos = i;				
 			}
 			else{
 				MAX = a[i];
-				maxPos = i;
+				if((MAX-MIN)>maxDiff){
+					maxPos = i;
+					minPos = tmpMinPos;
+					maxDiff = MAX - MIN;
+				}
+				
 			}
 		}
 		System.out.println(minPos + ", " + maxPos);
-		return (minPos < maxPos) ? (MAX - MIN) : 0;
+		return maxDiff;
 	}
 	
 	public static void main(String[] args) {
@@ -28,7 +34,8 @@ public class MaxDiff {
 		System.out.println(findMaxDiff(new int[]{5, 1, 3, 2, 4}));
 		System.out.println(findMaxDiff(new int[]{5, 3, 2, 1, 4}));
 		System.out.println(findMaxDiff(new int[]{3, 4, 1, 2, 5}));
-		System.out.println(findMaxDiff(new int[]{3, 5, 2, 1}));   // Failing
+		System.out.println(findMaxDiff(new int[]{3, 5, 2, 1})); 
+		System.out.println(findMaxDiff(new int[]{2, 3, 10, 6, 4, 8, 1}));   
 	}
 
 }
