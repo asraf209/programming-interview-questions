eb5cc25bce4b58b9140574dc8a3ff8e6a8e41f38
Asraf <asraf.alom02@gmail.com>
2016-06-01 22:54:50 -0500

    added output scenarios

--- src/algorithms/maxdiff/MaxDiff.java
+++ src/algorithms/maxdiff/MaxDiff.java
@@ -5,6 +5,9 @@ public class MaxDiff {
 	/**
 	 * Find pair of MIN and MAX. If their difference is larger than maximum difference,
 	 * then they are the possible candidate for output.
+	 * Two things to track: 
+	 * 		1) Max diff found so far
+	 * 		2) Min number found so far
 	 * 
 	 * @param a; An Array of integer numbers
 	 * @return Maximum difference between the MIN and MAX value, such that, 
@@ -37,14 +40,18 @@ public class MaxDiff {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(findMaxDiff(new int[]{1, 2, 3, 4, 5}));
-		System.out.println(findMaxDiff(new int[]{5, 4, 3, 2, 1}));
-		System.out.println(findMaxDiff(new int[]{5, 1, 3, 2, 4}));
-		System.out.println(findMaxDiff(new int[]{5, 3, 2, 1, 4}));
-		System.out.println(findMaxDiff(new int[]{3, 4, 1, 2, 5}));
-		System.out.println(findMaxDiff(new int[]{3, 5, 2, 1})); 
-		System.out.println(findMaxDiff(new int[]{2, 3, 10, 6, 4, 8, 1}));
-		System.out.println(findMaxDiff(new int[]{2, 3, 10, 1, 6, 4, 15}));
+		System.out.println(findMaxDiff(new int[]{1, 2, 3, 4, 5}));	// minI:0, maxI:4 -> diff:4
+		System.out.println(findMaxDiff(new int[]{5, 4, 3, 2, 1}));	// No satisfied min-max
+		System.out.println(findMaxDiff(new int[]{5, 1, 3, 2, 4}));  // minI:1, maxI:4 -> diff:3
+		System.out.println(findMaxDiff(new int[]{5, 3, 2, 1, 4}));  // minI:3, maxI:4 -> diff:3
+		System.out.println(findMaxDiff(new int[]{3, 4, 1, 2, 5}));  // minI:2, maxI:4 -> diff:4
+		
+		System.out.println(findMaxDiff(new int[]{3, 5, 2, 1}));  	// minI:0, maxI:1 -> diff:2 
+		
+		System.out.println(findMaxDiff(new int[]{2, 3, 10, 6, 4, 8, 1}));  	// minI:1, maxI:4 -> diff:3
+		System.out.println(findMaxDiff(new int[]{2, 3, 10, 1, 6, 4, 15}));  // minI:3, maxI:6 -> diff:14
+		
+		System.out.println(findMaxDiff(new int[]{2, 2, 10, 6, 4, 8, 1}));  	// minI:0, maxI:2 -> diff:8
 	}
 
 }
