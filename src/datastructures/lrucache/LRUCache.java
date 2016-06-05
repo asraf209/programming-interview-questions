1a522b29b13ae77e166138c138721886d98dc81a
Asraf <asraf.alom02@gmail.com>
2016-06-05 17:35:09 -0500

    added node class

--- src/datastructures/lrucache/LRUCache.java
+++ src/datastructures/lrucache/LRUCache.java
@@ -1,7 +1,15 @@
 package datastructures.lrucache;
 
 public class LRUCache {
-
+	class Node{
+		int key;
+		int value;
+		public Node(int key, int value){
+			this.key = key;
+			this.value = value;
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
