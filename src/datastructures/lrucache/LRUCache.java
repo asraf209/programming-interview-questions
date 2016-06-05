9d237a2764270bcdea3b17845c2cd9aca4710943
Asraf <asraf.alom02@gmail.com>
2016-06-05 18:55:01 -0500

    made Node as outer class

--- src/datastructures/lrucache/LRUCache.java
+++ src/datastructures/lrucache/LRUCache.java
@@ -3,21 +3,7 @@ package datastructures.lrucache;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LRUCache {
-	class Node{
-		int key;
-		int value;
-		Node prev;
-		Node next;		
-		
-		public Node(int key, int value){
-			this.key = key;
-			this.value = value;
-			this.prev = null;
-			this.next = null;
-		}
-	}
-	
+public class LRUCache {	
 	int capcacity;
 	int count;
 	Node head;
