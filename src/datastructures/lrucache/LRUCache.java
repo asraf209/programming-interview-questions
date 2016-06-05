3b8d6ff9f8923a3708bfa9b3c1aad5be08e89bab
Asraf <asraf.alom02@gmail.com>
2016-06-05 17:38:54 -0500

    added isempty, isfull and constructor

--- src/datastructures/lrucache/LRUCache.java
+++ src/datastructures/lrucache/LRUCache.java
@@ -10,6 +10,21 @@ public class LRUCache {
 		}
 	}
 	
+	int capcacity;
+	int count;
+	Node head;
+	Node tail;
+	
+	public LRUCache(int capacity){
+		this.capcacity = capacity;
+		this.count = 0;
+		this.head = null;
+		this.tail = null;
+	}
+	
+	public boolean isEmpty()	{return this.count == 0; }
+	public boolean isFull()		{return this.count == this.capcacity; }
+			
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
