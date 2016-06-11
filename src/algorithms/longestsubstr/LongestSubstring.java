cbe3ed64f667c6f92740ad98dd09778354238270
Asraf <asraf.alom02@gmail.com>
2016-06-11 13:20:02 -0500

    initial commit

---
+++ src/algorithms/longestsubstr/LongestSubstring.java
@@ -0,0 +1,13 @@
+package algorithms.longestsubstr;
+
+/**
+ * Longest substring with non-repeating characters
+ *
+ */
+public class LongestSubstring {
+
+	public static void main(String[] args) {
+
+	}
+
+}
