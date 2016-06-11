dbdfda11c651602f0491ce941d904d2a68933f96
Asraf <asraf.alom02@gmail.com>
2016-06-11 14:23:18 -0500

    found another approach

--- src/algorithms/longestsubstr/LongestSubstring.java
+++ src/algorithms/longestsubstr/LongestSubstring.java
@@ -1,58 +1,38 @@
 package algorithms.longestsubstr;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Longest substring with non-repeating characters
  *
  */
-public class LongestSubstring {
-	public static int longestSubstring2(String s){
+public class LongestSubstring {	
+	
+	public static int longestSubstring(String s){
 		if(s==null || s.trim().isEmpty())	return 0;		
 		
 		s = s.trim();		
 		int start = 0, tmpStart = 0;
 		int length = 0, maxLength = 0;
-		boolean[] chSet = new boolean[256];
+		Map<Character, Boolean> map = new HashMap<>();
 		
-		for(int i=0; i<s.length(); i++){
-			int index = s.charAt(i);
-			if(chSet[index]){
+		for(int i=0; i<s.length(); i++){			
+			if(map.containsKey(s.charAt(i))){				
 				if(length > maxLength){
 					start = tmpStart;
 					maxLength = length;
 				}
-				tmpStart = i+1;
-			}
-			else{
-				length++;
-				chSet[index] = true;
-			}
-		}
-		System.out.println(s.substring(start, start+maxLength));
-		return maxLength;
-	}
-	
-	public static int longestSubstring(String s){
-		if(s==null || s.trim().isEmpty())	return 0;				
-		if(s.trim().length() < 2)	return 1;
-		
-		s = s.trim();				
-		int start = 0, tmpStart = 0;
-		int length = 1, maxLength = 1;
-		
-		for(int i=1; i<s.length(); i++){
-			if(s.charAt(i-1) == s.charAt(i)){
-				if(length > maxLength){
-					start = tmpStart;
-					maxLength = length;
-				}
+				map.clear();
+				map.put(s.charAt(i), true);
 				tmpStart = i;
 				length = 1;
 			}
 			else{
 				length++;
+				map.put(s.charAt(i), true);
 			}
 		}
-		
 		if(length > maxLength){
 			start = tmpStart;
 			maxLength = length;
@@ -64,7 +44,7 @@ public class LongestSubstring {
 	
 	public static void main(String[] args) {
 		System.out.println(longestSubstring("pwwke"));
-		System.out.println(longestSubstring("abcabcbb"));
+		System.out.println(longestSubstring("abcabcd"));
 		System.out.println(longestSubstring("bbbb"));
 	}
 
