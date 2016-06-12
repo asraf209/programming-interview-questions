a3bd0a22593a8ffde9f9111702c4db7d4906c0f3
Asraf <asraf.alom02@gmail.com>
2016-06-12 00:29:45 -0500

    made max heap

--- src/datastructures/heapusingqueue/Heap.java
+++ src/datastructures/heapusingqueue/Heap.java
@@ -19,10 +19,26 @@ public class Heap {
 		System.out.println(minHeap);
 	}
 	
+	public static void makeMaxHeap(int[] a, int SIZE){
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(SIZE, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1; 
+			}
+		});
+		
+		for(int n : a){
+			maxHeap.offer(n);
+		}
+		
+		System.out.println(maxHeap);
+	}
+	
 	public static void main(String[] args) {	
 		int SIZE = 10;
 		int[] a = { 1, 10, 5, 3, 4, 7, 6, 9, 8 };
 		makeMinHeap(a, SIZE);
+		makeMaxHeap(a, SIZE);
 	}
 
 }
