df0dc5cb1d6b23365513b3d05790a15f92757e9f
Asraf <asraf.alom02@gmail.com>
2016-06-14 19:30:22 +0000

    added reverseEachWordInSentence

--- src/algorithms/reverseEachWord/ReverseEachWord.java
+++ src/algorithms/reverseEachWord/ReverseEachWord.java
@@ -1,8 +1,27 @@
 package algorithms.reverseEachWord;
 
-
-
 public class ReverseEachWord {
+
+	public static String reverseEachWordInSentence(String sentence){
+		if(sentence==null || sentence.length()<2)
+			return sentence;
+		
+		char[] chars = sentence.toCharArray();
+		int s=0, e=0;
+		while(e<chars.length){
+			if(chars[e]==' ' || chars[e]=='\t'){
+				if((e-s)>0)	
+					reverse(chars, s, e-1);
+				s = e+1;
+			}
+			e++;
+		}
+
+		if((e-s)>0)	
+			reverse(chars, s, e-1);
+
+		return new String(chars);
+	}
 
 	private static void reverse(char[] sentence, int s, int e){
 		if(sentence==null || sentence.length<2)
