b2ceb3d30148adc546cb48e76bb918329cb5ab11
Asraf <asraf.alom02@gmail.com>
2016-06-14 00:25:07 -0500

    sample data

--- src/algorithms/topncustomers/TopNCustomers.java
+++ src/algorithms/topncustomers/TopNCustomers.java
@@ -3,7 +3,9 @@ package algorithms.topncustomers;
 public class TopNCustomers {
 
 	public static void main(String[] args) {
-		String[] names = {"bob", "sam", "smith", "alan"};
+		int N = 3;
+		String[] names = {"bob", "sam", "smith", "alan", "sam", "sam", "kobi", "sam", "bob", "alan"};
+		int[] expenses = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
 	}
 
 }
