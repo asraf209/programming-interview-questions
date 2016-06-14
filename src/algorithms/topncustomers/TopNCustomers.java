daf014f8a07ef8e0348f27eb5af590e8636d8bbc
Asraf <asraf.alom02@gmail.com>
2016-06-14 00:41:09 -0500

    Made the Heap

--- src/algorithms/topncustomers/TopNCustomers.java
+++ src/algorithms/topncustomers/TopNCustomers.java
@@ -1,12 +1,15 @@
 package algorithms.topncustomers;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
 
 public class TopNCustomers {
 
-	public static List<String> findTopNCustomer(String[] names, int[] expenses){
+	public static List<String> findTopNCustomer(String[] names, int[] expenses, int N){
 		if(names==null || names.length==0 || expenses==null || expenses.length==0)
 			return null;
 		if(names.length!=expenses.length)	return null;
@@ -18,7 +21,20 @@ public class TopNCustomers {
 			else
 				expenseInfo.put(names[i], expenses[i]);
 		}
-		System.out.println(expenseInfo);
+		System.out.println("Expense Info: " + expenseInfo);
+		
+		PriorityQueue<Entry<String, Integer>> heap = new PriorityQueue<>(N, new Comparator<Entry<String, Integer>>() {
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+				return o1.getValue() - o2.getValue();
+			}
+		});
+		
+		for(Entry<String, Integer> customer : expenseInfo.entrySet()){
+			heap.offer(customer);
+		}
+		System.out.println(heap);
+		
 		return null;
 	}
 	
@@ -27,7 +43,7 @@ public class TopNCustomers {
 		String[] names = {"bob", "sam", "smith", "alan", "sam", "sam", "kobi", "sam", "bob", "alan"};
 		int[] expenses = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
 		
-		System.out.println(findTopNCustomer(names, expenses));
+		System.out.println(findTopNCustomer(names, expenses, N));
 	}
 
 }
