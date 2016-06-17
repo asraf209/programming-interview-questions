b5f6de8f5b6df3524675e9490a3b37cd8443311f
Asraf <asraf.alom02@gmail.com>
2016-06-17 01:10:41 -0500

    added get metod for open addressing

--- src/datastructures/mapWithCollision/MapOpenAddressing.java
+++ src/datastructures/mapWithCollision/MapOpenAddressing.java
@@ -9,6 +9,24 @@ public class MapOpenAddressing {
 		map = new Entry[this.SIZE];
 	}
 	
+	public int get(int key){
+		int hash = (key % SIZE);
+		if(map[hash] == null)
+			return Integer.MIN_VALUE;
+		
+		if(map[hash].key == key)
+			return map[hash].value;
+		else{
+			int pointer = (hash + 1) % SIZE;
+			while(pointer!=hash && map[pointer].key!=key){
+				pointer = (pointer + 1)%SIZE;
+			}
+			
+			if(pointer==hash)	return Integer.MIN_VALUE;			
+			return map[pointer].value;
+		}				
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
