0235904a3388354d47161b40a048ee52a4166d63
Asraf <asraf.alom02@gmail.com>
2016-06-17 00:24:42 -0500

    added Entry class

---
+++ src/datastructures/mapWithCollision/Entry.java
@@ -0,0 +1,10 @@
+package datastructures.mapWithCollision;
+
+public class Entry {
+	int key;
+	int value;
+	public Entry(int key, int value){
+		this.key = key;
+		this.value = value;
+	}
+}
--- src/datastructures/mapWithCollision/MapChaining.java
+++ src/datastructures/mapWithCollision/MapChaining.java
@@ -1,7 +1,11 @@
 package datastructures.mapWithCollision;
 
 public class MapChaining {
-
+	
+	public void get(int key){
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
