915d648082c780546158f1a95402303fb085f967
Asraf <asraf.alom02@gmail.com>
2016-06-16 20:11:23 -0500

    implemented algorithm

--- src/algorithms/evaluateRPN/EvaluateRPN.java
+++ src/algorithms/evaluateRPN/EvaluateRPN.java
@@ -1,9 +1,46 @@
 package algorithms.evaluateRPN;
 
+import java.util.Stack;
+
 public class EvaluateRPN {
 	
-	public static int evaluate(String[] expr){
-		return 0;
+	public static int evaluate(char[] expr){
+		if(expr == null || expr.length==0)	return 0;
+		
+		int a, b;
+		Stack<Integer> stack = new Stack<Integer>();		
+		for(char ch : expr){
+			switch(ch){
+				case '/':
+					b = stack.pop();
+					a = stack.pop();
+					stack.push(a/b);
+					break;
+					
+				case '*':
+					b = stack.pop();
+					a = stack.pop();
+					stack.push(a*b);
+					break;
+					
+				case '+':
+					b = stack.pop();
+					a = stack.pop();
+					stack.push(a+b);
+					break;
+					
+				case '-':
+					b = stack.pop();
+					a = stack.pop();
+					stack.push(a-b);
+					break;
+					
+				default:
+					stack.push(ch - '0');
+					break;
+			}					
+		}
+		return stack.pop();
 	}
 	
 	public static void main(String[] args) {	
