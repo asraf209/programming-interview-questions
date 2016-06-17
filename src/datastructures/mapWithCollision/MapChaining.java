6caf5eb71e2426133205f689dde9711c375907c3
Asraf <asraf.alom02@gmail.com>
2016-06-17 00:22:37 -0500

    working with Map considering collision

---
+++ src/datastructures/mapWithCollision/MapChaining.java
@@ -0,0 +1,10 @@
+package datastructures.mapWithCollision;
+
+public class MapChaining {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/datastructures/mapWithCollision/MapOpenAddressing.java
@@ -0,0 +1,10 @@
+package datastructures.mapWithCollision;
+
+public class MapOpenAddressing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
