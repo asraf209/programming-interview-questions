3005dadf0d3c707534881f1f11cbbfd7ae3d9909
Asraf <asraf.alom02@gmail.com>
2016-06-17 00:35:03 -0500

    get() for Chainging approach

--- src/datastructures/mapWithCollision/Entry.java
+++ src/datastructures/mapWithCollision/Entry.java
@@ -3,8 +3,11 @@ package datastructures.mapWithCollision;
 public class Entry {
 	int key;
 	int value;
+	Entry next;
+	
 	public Entry(int key, int value){
 		this.key = key;
 		this.value = value;
+		this.next = null;
 	}
 }
--- src/datastructures/mapWithCollision/MapChaining.java
+++ src/datastructures/mapWithCollision/MapChaining.java
@@ -2,13 +2,34 @@ package datastructures.mapWithCollision;
 
 public class MapChaining {
 	
-	public void get(int key){
+	private int SIZE;
+	private Entry[] map;
+	
+	public MapChaining(int SIZE){
+		this.SIZE = SIZE;
+		map = new Entry[this.SIZE];
+	}
+	
+	public int get(int key){
+		int hash = (key % SIZE);
+		if(map[hash] == null)
+			return Integer.MIN_VALUE;
 		
+		Entry head = map[hash];
+		while(head != null){
+			if(head.key == key)
+				return head.value;
+			head = head.next;
+		}
+		
+		return Integer.MIN_VALUE;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int SIZE = 10;
+		MapChaining map = new MapChaining(SIZE);
+		System.out.println(map.get(100));
+		
 	}
 
 }
