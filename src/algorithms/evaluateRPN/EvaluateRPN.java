cfe2c56bec956e9f51d5799e011c60b1546671bd
Asraf <asraf.alom02@gmail.com>
2016-06-16 20:08:27 -0500

    initial commit

---
+++ src/algorithms/evaluateRPN/EvaluateRPN.java
@@ -0,0 +1,13 @@
+package algorithms.evaluateRPN;
+
+public class EvaluateRPN {
+	
+	public static int evaluate(String[] expr){
+		return 0;
+	}
+	
+	public static void main(String[] args) {	
+
+	}
+
+}
