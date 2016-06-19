85f801d184a686f6c762e8749c08d16d18e392a8
Asraf <asraf.alom02@gmail.com>
2016-06-19 13:15:35 -0500

    Refactored and put some comments

--- src/string/permutations/Permutations.java
+++ src/string/permutations/Permutations.java
@@ -2,6 +2,12 @@ package string.permutations;
 
 public class Permutations {
 	
+	private static void swap(char[] str, int a, int b){
+		char c = str[a];
+		str[a] = str[b];
+		str[b] = c;
+	}
+	
 	public static void permute(char[] str, int s, int e){
 		if(s==e){
 			System.out.println(new String(str));			
@@ -15,15 +21,15 @@ public class Permutations {
 		}
 	}
 	
-	private static void swap(char[] str, int a, int b){
-		char c = str[a];
-		str[a] = str[b];
-		str[b] = c;
+	public static void printAllPermutations(String s){
+		char[] str = s.toCharArray();
+		permute(str, 0, str.length-1);
+		System.out.println("\n");
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {		
+		String s = "ABC";
+		printAllPermutations(s);				
 	}
 
 }
--- src/string/permutations/README.md
+++ src/string/permutations/README.md
@@ -2,7 +2,6 @@
 
 Print all the permutations of a given String. Given, all characters are unique
 
-
 ## Example
 For input String "ABC", there are 6 permutations (n!)
 
@@ -10,3 +9,8 @@ ABC ACB BAC BCA CBA CAB
 
 ## Time Complexity
 O(!n)
+
+### Note
+This program will also work for duplicate characters. We only need to use a Set. So whenever we print a String we check in the Set. If it is not there then print, otherwise discard
+
+Image: permutations.gif
