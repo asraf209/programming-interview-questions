504d80acdbc2fdbae1b4b87ef2bc6d5543ed7ece
Asraf <asraf.alom02@gmail.com>
2016-06-19 13:06:50 -0500

    implemented algo

--- src/string/permutations/Permutations.java
+++ src/string/permutations/Permutations.java
@@ -2,13 +2,28 @@ package string.permutations;
 
 public class Permutations {
 	
-	public static void permute(char[] str){
-		
+	public static void permute(char[] str, int s, int e){
+		if(s==e){
+			System.out.println(new String(str));			
+		}
+		else{
+			for(int i=s; i<=e; i++){
+				swap(str, s, i);
+				permute(str, s+1, e);
+				swap(str, s, i);
+			}
+		}
+	}
+	
+	private static void swap(char[] str, int a, int b){
+		char c = str[a];
+		str[a] = str[b];
+		str[b] = c;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
