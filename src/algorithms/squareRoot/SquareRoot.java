ad386ebfb9c913ce2930385fd177f3afa9b7eafc
Asraf <asraf.alom02@gmail.com>
2016-06-25 14:47:51 -0500

    Done with square root

--- src/algorithms/squareRoot/SquareRoot.java
+++ src/algorithms/squareRoot/SquareRoot.java
@@ -1,9 +1,33 @@
 package algorithms.squareRoot;
 
 public class SquareRoot {
-
-	public static void main(String[] args) {
+	
+	public static int getSquareRoot(int num){
+		if(num < 0)	return -1;
 		
+		int root=0;		
+		int out=root;
+		while(root*root <= num){
+			out = root;
+			root++;
+		}
+		return out;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getSquareRoot(0));
+		System.out.println(getSquareRoot(1));
+		System.out.println(getSquareRoot(2));
+		System.out.println(getSquareRoot(3));
+		System.out.println(getSquareRoot(4));
+		System.out.println(getSquareRoot(5));
+		System.out.println(getSquareRoot(6));
+		System.out.println(getSquareRoot(7));
+		System.out.println(getSquareRoot(8));
+		System.out.println(getSquareRoot(9));
+		System.out.println(getSquareRoot(10));
+		System.out.println(getSquareRoot(11));
+		System.out.println(getSquareRoot(12));
 	}
 
 }
