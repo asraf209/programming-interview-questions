4eed37ebc4b15fd883413964afcaadad8f3f0c6a
Asraf <asraf.alom02@gmail.com>
2016-06-27 01:03:57 -0500

    Added search, insert and remove method

--- src/datastructures/prefixTree/PrefixTree.java
+++ src/datastructures/prefixTree/PrefixTree.java
@@ -11,4 +11,58 @@ public class PrefixTree {
 	}
 	
 	Node root = new Node();
+	
+	public boolean isPresent(String word){
+		if(word==null || word.trim().isEmpty())	return false;
+		
+		Node current = root;
+		for(char ch : word.trim().toCharArray()){
+			if(current.children.containsKey(ch))
+				current = current.children.get(ch);			
+			else 
+				return false;
+		}
+		return current.isWord;
+	}
+	
+	public void insert(String word){
+		if(word==null || word.trim().isEmpty())	return;
+		if(isPresent(word))	return;
+		
+		Node current = root;
+		for(char ch : word.trim().toCharArray()){
+			if(current.children.containsKey(ch)){
+				Node child = current.children.get(ch);
+				child.prefix++;
+				current.children.put(ch, child);
+				current = child;
+			}
+			else{
+				Node child = new Node();
+				child.prefix++;
+				current.children.put(ch, child);
+				current = child;
+			}
+			
+		}
+		current.isWord = true;
+	}
+	
+	public void remove(String word){
+		if(word==null || word.trim().isEmpty())	return;
+		if(!isPresent(word))	return;
+		
+		Node current = root;
+		for(char ch : word.trim().toCharArray()){
+			Node child = current.children.get(ch);
+			if(child.prefix > 1)
+				child.prefix--;
+			else{
+				current.children.remove(ch);
+				return;
+			}
+			current = child;
+		}
+		current.isWord = false;
+	}
 }
