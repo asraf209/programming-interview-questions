ce6864c0a6c9b45c8481c06dbca9c9598666b7c6
Asraf <asraf.alom02@gmail.com>
2016-06-28 22:34:02 -0500

    Traverse a binary tree in spiral order

---
+++ src/tree/traverseBTSpiralOrder/README.md
@@ -0,0 +1,9 @@
+# Traverse a Binary Tree in Spiral order
+
+Write a function to print spiral order traversal of a tree. 
+
+## Example
+
+Input: [Binary Tree](./spiral_order.gif)
+
+For the above tree, function should print 1, 2, 3, 4, 5, 6, 7.
---
+++ src/tree/traverseBTSpiralOrder/TraverseBTInSpiralOrder.java
@@ -0,0 +1,17 @@
+package tree.traverseBTSpiralOrder;
+
+public class TraverseBTInSpiralOrder {
+	
+	class Node{
+		int value;
+		public Node(int value){
+			this.value = value;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
