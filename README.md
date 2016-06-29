56b3f7ca6d91290ab0667074a77ad462f484e277
Asraf <asraf.alom02@gmail.com>
2016-06-28 23:24:14 -0500

    Stack approach

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Helping people in preparing for Programming Interviews
 
 |#|Title|Category|
 |---|-----|---------|
+|30|[Reverse K Nodes](./src/linkedlist/reverseKNodes)|LinkedList|
 |29|[Traverse BT in Spiral](./src/tree/traverseBTSpiralOrder)|Tree|
 |28|[Auto-Complete Suggestions](./src/tree/autoComplete)|Tree|
 |27|[Prefix Tree](./src/datastructures/prefixTree)|Data Structure|
--- src/linkedlist/reverseKNodes/ReverseKNodes.java
+++ src/linkedlist/reverseKNodes/ReverseKNodes.java
@@ -1,5 +1,7 @@
 package linkedlist.reverseKNodes;
 
+import java.util.Stack;
+
 public class ReverseKNodes {
 	class Node{
 		int value;
@@ -24,6 +26,46 @@ public class ReverseKNodes {
 			}
 		}
 		return out;
+	}		
+	
+	/**
+	 * Using Stack. Read elements and push into Stack.
+	 * Then read back from Stack and make a list
+	 * Time: O(n)
+	 * Space: O(n)
+	 * @param head
+	 * @param k
+	 * @return
+	 */
+	public Node reverseKNodesFromHead(Node head, int k){
+		if(head==null || head.next==null || k<2)
+			return head;
+		
+		Stack<Integer> stack = new Stack<>();
+		Node curr = head;
+		int count=0;
+		while(curr!=null && count<k){
+			stack.push(curr.value);
+			curr = curr.next;
+			count++;
+		}
+		
+		Node newHead = null;
+		Node tail = newHead;				
+		while(!stack.isEmpty()){
+			Node n = new Node(stack.pop());
+			if(newHead==null){
+				newHead = n;
+				tail = n;
+			}
+			else{
+				tail.next = n;
+				tail = tail.next;
+			}
+		}
+		tail.next = curr;
+		
+		return newHead;
 	}
 	
 	public static void printList(Node head){
@@ -39,6 +81,10 @@ public class ReverseKNodes {
 		ReverseKNodes reverseKNodes = new ReverseKNodes();
 		Node head = reverseKNodes.makeAList(a);
 		printList(head);
+		
+		int k = 30;
+		Node newHead = reverseKNodes.reverseKNodesFromHead(head, k);
+		printList(newHead);
 	}
 
 }
