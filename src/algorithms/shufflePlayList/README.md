cc2282fbd4612b3591cfc7f0800653624471bbe4
Asraf <asraf.alom02@gmail.com>
2016-06-28 23:52:56 -0500

    done with sonf shuffling

--- src/algorithms/shufflePlayList/README.md
+++ src/algorithms/shufflePlayList/README.md
@@ -6,3 +6,5 @@ Given a list of songs. How would you shuffle it?
 ## Solution
 
 This is like shuffling a deck of Cards or Array elements. 
+
+The song that picked up randomly in each iteration should not be considered further. 
--- src/algorithms/shufflePlayList/ShufflePlayList.java
+++ src/algorithms/shufflePlayList/ShufflePlayList.java
@@ -2,10 +2,31 @@ package algorithms.shufflePlayList;
 
 public class ShufflePlayList {
 
-	
+	/**
+	 * O(n); n - Size of the playlist
+	 * @param playList
+	 * @return
+	 */
+	public static String[] shufflePlayList(String[] playList){
+		if(playList==null || playList.length<2)	return playList;
+				
+		for(int i=0; i<playList.length; i++){
+			int nextSong = (int) (i + Math.random()*(playList.length - i));
+			
+			// Swap song
+			String tmp = playList[i];
+			playList[i] = playList[nextSong];
+			playList[nextSong] = tmp;
+		}
+		return playList;
+	}
 	
 	public static void main(String[] args) {
-		String[] playlist = {"Song1", "Song2", "Song3", "Song4", "Song5"};
+		String[] playList = {"Song1", "Song2", "Song3", "Song4", "Song5"};
+		playList = shufflePlayList(playList);
+		for(String song : playList)
+			System.out.print(song + ", ");
+		System.out.println();
 	}
 
 }
