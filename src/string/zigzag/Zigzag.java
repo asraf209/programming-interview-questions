a4cee0bc1d3e03d327ce38614593ce558da71bbb
Asraf <asraf.alom02@gmail.com>
2016-07-11 00:46:04 -0500

    problem description

---
+++ src/string/zigzag/Zigzag.java
@@ -0,0 +1,17 @@
+package string.zigzag;
+
+public class Zigzag {
+	public static String convert(String text, int nRows){
+		if(text==null || text.trim().isEmpty() || nRows<2)	return text;
+		
+		StringBuffer buffer = new StringBuffer();
+		
+		return buffer.toString();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
