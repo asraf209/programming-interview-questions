fad390abe7d294edc1c8113325ee7ca062c777dd
Asraf <asraf.alom02@gmail.com>
2016-07-17 19:43:24 -0500

    done with zigzag convertion

--- src/string/zigzag/Zigzag.java
+++ src/string/zigzag/Zigzag.java
@@ -1,28 +1,37 @@
 package string.zigzag;
 
 public class Zigzag {
-	public static String convert(String text, int nRows){
+	
+	public static String zigzagConvertion(String text, int nRows){
 		if(text==null || text.trim().isEmpty() || nRows<2)	return text;
 		
 		StringBuffer buffer = new StringBuffer();
-		int r=1;
-		while(r<=nRows){
-			int index = r-1;
-			while(index<text.trim().length()){
-				System.out.print(index);
-				buffer.append(text.trim().charAt(index));
-				index = index + (nRows-r) + (nRows-r);				
+		for(int r=0; r<nRows; r++){			
+			int i = r;
+			int count = 1;
+			while(i<text.length()){				
+				buffer.append(text.charAt(i));	
+				/**
+				 * For the 1st and last row, index of next element is 2*(lastRow-firstRow)
+				 */
+				if(r==0 || r==nRows-1)			
+					i = i + 2*(nRows-1);
+				/**
+				 * For others, find distance from current position to the next boundary row(1st/last). 
+				 * Count is actually used here to compute that boundary index
+				 */
+				else{
+					i = i + 2*(count*(nRows-1) - i);
+					count++;
+				}
 			}
-			r++;
-			System.out.println();
 		}
-		
 		return buffer.toString();
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(convert("ABCDEFGHIJKLM", 4));
-
+		System.out.println(zigzagConvertion("PAYPALISHIRING", 3));
+		System.out.println(zigzagConvertion("PAYPALISHIRING", 4));
 	}
 
 }
