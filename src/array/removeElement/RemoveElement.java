f0be2ac6aa9df9a420eddc59298fde48ce37bcb7
Asraf <asraf.alom02@gmail.com>
2016-07-23 01:14:31 -0500

    Added problem description for removeElement

---
+++ src/array/removeElement/RemoveElement.java
@@ -0,0 +1,12 @@
+package array.removeElement;
+
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+        
+    }
+
+	public static void main(String[] args) {
+
+	}
+
+}
