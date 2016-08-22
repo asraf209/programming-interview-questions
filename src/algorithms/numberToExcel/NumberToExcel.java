9538833e60ba4e8575c81c4f3ad709164e93ff3c
Asraf <asraf.alom02@gmail.com>
2016-08-21 23:59:50 -0500

    fixed algorithm for edge cases; like 52

--- src/algorithms/numberToExcel/NumberToExcel.java
+++ src/algorithms/numberToExcel/NumberToExcel.java
@@ -12,27 +12,37 @@ public class NumberToExcel {
 		
 		// Preprocessing step.
 		char base = 'A';
-		for(int k=1; k<26; k++){
+		for(int k=1; k<=26; k++){
 			map.put(k, (char) (base+k-1));
-		}
-		map.put(0, 'Z');
+		}		
 	}
 	
 	public String getNumberToExcelColumn(int num){
 		if(num < 1)	return "";
 		
 		String out = "";
-		while(num/26 > 0){
+		while(num > 0){
 			int remainder = num % 26;
-			out = map.get(remainder) + out;
 			num = num / 26;
+			
+			// For 26 the remainder will be 0, which is equivalent to 'Z'
+			if(remainder==0){
+				out = map.get(26) + out;
+				num = num - 1;
+			}
+			else
+				out = map.get(remainder) + out;			
 		}
 		
 		return out;
 	}
 	
 	public static void main(String[] args) {
-		
+		NumberToExcel numberToExcel = new NumberToExcel();		
+		System.out.println(numberToExcel.getNumberToExcelColumn(19));
+		System.out.println(numberToExcel.getNumberToExcelColumn(26));
+		System.out.println(numberToExcel.getNumberToExcelColumn(52));
+		System.out.println(numberToExcel.getNumberToExcelColumn(704));
 	}
 
 }
