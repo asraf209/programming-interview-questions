5de43a5eefa4ae9b9a1523905f120bc035448c58
Asraf <asraf.alom02@gmail.com>
2016-08-27 18:31:17 -0500

    implemedted isPrime method

--- src/algorithms/nPrimeNumbers/NPrimeNumbers.java
+++ src/algorithms/nPrimeNumbers/NPrimeNumbers.java
@@ -1,10 +1,24 @@
 package algorithms.nPrimeNumbers;
 
+import java.util.Scanner;
+
 public class NPrimeNumbers {
 	
+	public static boolean isPrime(int num){
+		if(num<1)	return false;
+		if(num<4)	return true;
+		
+		if(num%2==0)	return false;
+		
+		for(int n=3; (n*n)<=num; n+=2){
+			if(num%n==0)	return false;
+		}
+		
+		return true;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+				
 	}
 
 }
