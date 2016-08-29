ac162d29447e437f82fe063a5f629ddc13f4fabe
Asraf <asraf.alom02@gmail.com>
2016-08-28 20:04:00 -0500

    implemented faster one

--- src/string/firstNonRepeatChar/FristNonRepeat.java
+++ src/string/firstNonRepeatChar/FristNonRepeat.java
@@ -1,6 +1,7 @@
 package string.firstNonRepeatChar;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -27,9 +28,35 @@ public class FristNonRepeat {
 		
 		return '\0';
 	}
+		
+	/**
+	 * This is faster than the previous one. 
+	 * Here we are not going over the whole text. But only over the map.
+	 * Use case: Imagine a text of thousands chars with only 4 non repeating chars
+	 */
+	public static char findFirstNonRepeatFast(String s){
+		if(s==null || s.isEmpty())	return '\0';
+		Map<Character, Integer>	map = new LinkedHashMap<>();
+		for(char ch : s.toCharArray()){
+			if(map.containsKey(ch)){
+				int count = map.get(ch);
+				map.put(ch, count+1);
+			}
+			else
+				map.put(ch, 1);
+		}
+		
+		for(char key : map.keySet()){
+			if(map.get(key)==1)
+				return key;
+		}
+			
+		return '\0';
+	}
 	
 	public static void main(String[] args) {		
 		System.out.println(findFirstNonRepeat("total"));
+		System.out.println(findFirstNonRepeatFast("total"));
 	}
 
 }
