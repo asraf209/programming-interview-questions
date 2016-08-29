45c9e9fa09fb8b1f14cf332c66293c7458832b65
Asraf <asraf.alom02@gmail.com>
2016-08-28 19:57:18 -0500

    implemented the trivial one

--- src/string/firstNonRepeatChar/FristNonRepeat.java
+++ src/string/firstNonRepeatChar/FristNonRepeat.java
@@ -1,10 +1,35 @@
 package string.firstNonRepeatChar;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Trivial one. 
+ * O(2n)
+ */
 public class FristNonRepeat {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static char findFirstNonRepeat(String s){
+		if(s==null || s.isEmpty())	return '\0';
+		Map<Character, Integer> map = new HashMap<>();
+		for(char ch : s.toCharArray()){
+			if(map.containsKey(ch)){
+				int count = map.get(ch);
+				map.put(ch, count+1);
+			}
+			else
+				map.put(ch, 1);
+		}
+		
+		for(char ch : s.toCharArray()){
+			if(map.get(ch)==1)
+				return ch;
+		}
+		
+		return '\0';
+	}
+	
+	public static void main(String[] args) {		
+		System.out.println(findFirstNonRepeat("total"));
 	}
 
 }
