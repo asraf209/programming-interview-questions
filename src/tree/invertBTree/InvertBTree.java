eb75f3fed87e27decf396a5107633117a47725bb
Asraf <asraf.alom02@gmail.com>
2016-10-05 00:07:47 -0500

    implemented algorithm

--- src/tree/invertBTree/InvertBTree.java
+++ src/tree/invertBTree/InvertBTree.java
@@ -9,6 +9,21 @@ public class InvertBTree {
 	    TreeNode(int x) { val = x; }
 	}
 	
+	public TreeNode invertBinaryTree(TreeNode root) {
+        if(root==null)  return root;
+        root = swap(root, root.left, root.right);
+        if(root.left!=null) invertBinaryTree(root.left);
+        if(root.right!=null) invertBinaryTree(root.right);
+        return root;
+    }
+    
+    public TreeNode swap(TreeNode root, TreeNode left, TreeNode right){
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        return root;
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
