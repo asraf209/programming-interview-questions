a731af49c99cdcae859f5d761beb4f51f765a271
Asraful Alom <asraf.alom02@gmail.com>
2021-04-15 00:30:21 -0700

    pwd, mkdir, cd, quit are working

--- src/datastructures/bashCmdProcessor/CommandProcessor.java
+++ src/datastructures/bashCmdProcessor/CommandProcessor.java
@@ -6,20 +6,67 @@ import java.io.InputStreamReader;
 
 //The idea is to create a Prefix Tree, where each Node will wither represent a Directory or File
 public class CommandProcessor {
-	Node root = new Node("root", true);
+	Node root = new Node("root" /*name*/, true /*isDirectory*/);
 	Node current = root;
+	
+	void currentDirectoryPath() {
+		String path = "";
+		Node curr = current;
+		while(curr != root) {
+			path = "/" + curr.name + path;
+			curr = curr.parent;
+		}
+		
+		path = "/" + curr.name + path;
+		
+		System.out.println(path);
+	}
+	
+	void makeDirectory(String name) {
+		if (current.child.containsKey(name)) {
+			System.out.println("Directory already exists");
+			return;
+		}
+		
+		Node newNode = new Node(name, true /*isDirectory*/);
+		newNode.parent = current;
+		current.child.put(name, newNode);
+	}
+	
+	void changeDirectory(String name) {
+		if (name.equals("..")) {
+			if (current != root)	current = current.parent;
+			return;
+		}
+		
+		if (!current.child.containsKey(name)) {
+			System.out.println("Directory not found");
+			return;
+		}
+		
+		current = current.child.get(name);
+	}
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String line;
+		CommandProcessor cmdProcessor = new CommandProcessor();
 		
 		while ((line = reader.readLine()) != null) {
 			line = line.trim().toLowerCase();
-			switch (line) {
+			String[] cmds = line.split(" ");
+			
+			switch (cmds[0]) {
 				case "quit":
 					return;
 				case "pwd":
-					System.out.println("Hello");
+					cmdProcessor.currentDirectoryPath();
+					break;
+				case "mkdir":
+					cmdProcessor.makeDirectory(cmds[1]);
+					break;
+				case "cd":
+					cmdProcessor.changeDirectory(cmds[1]);
 					break;
 				default:
 					return;
