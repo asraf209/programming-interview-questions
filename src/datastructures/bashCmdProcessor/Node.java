3fdbe6d5bd3b1025aefd743052d02e059975fe4d
Asraful Alom <asraf.alom02@gmail.com>
2021-04-14 23:49:07 -0700

    created Node class for the Trie

---
+++ src/datastructures/bashCmdProcessor/Node.java
@@ -0,0 +1,22 @@
+package datastructures.bashCmdProcessor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Node {
+	String name;
+	boolean isDirectory;
+	Node parent;
+	Map<String, Node> child;
+	
+	public Node(String name, boolean isDirectory, Node parent) {
+		this.name = name;
+		this.isDirectory = isDirectory;
+		this.parent = parent;
+		this.child = new HashMap<>();
+	}
+	
+	public Node(String name, boolean isDirectory) {
+		this(name, isDirectory, null);
+	}
+}
