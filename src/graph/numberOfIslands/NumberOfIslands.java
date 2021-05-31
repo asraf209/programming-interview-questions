873b9d4dee34b9a0f84a668c363158afbdc13c52
Asraful Alom <asraf.alom02@gmail.com>
2021-05-30 17:33:29 -0700

    Alternative approach of just checking surrounding (Left and Up)

--- src/graph/numberOfIslands/NumberOfIslands.java
+++ src/graph/numberOfIslands/NumberOfIslands.java
@@ -22,6 +22,28 @@ public class NumberOfIslands {
 		return count;
 	}
 	
+	// Alternative approach to just check Left and Up (surrounding)
+	public static int numberOfIslandsV2(int[][] grid) {
+		if (grid==null || grid.length==0 || grid[0].length==0)	return 0;
+		int count=0;
+		
+		for (int r=0; r<grid.length; r++) {
+			for (int c=0;c<grid[0].length; c++) {
+				if (grid[r][c] == 1 && !isSurroundingAnIsland(grid, r, c)) {
+					count++;
+				}
+			}
+		}
+		
+		return count;
+	}
+	
+	private static boolean isSurroundingAnIsland(int[][] grid, int r, int c) {
+		if (r>0 && grid[r-1][c] == 1)	return true;
+		if (c>0 && grid[r][c-1] == 1)	return true;
+		return false;
+	}
+	
 	private static void explore(int r, int c, int[][] grid, boolean[][] visited) {
 		if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]==0 || visited[r][c])	return;
 		
@@ -40,11 +62,13 @@ public class NumberOfIslands {
 						{1,1,0,0,0},
 						{0,0,0,0,0}};
 		System.out.println(numberOfIslands(grid1));
+		System.out.println(numberOfIslandsV2(grid1));
 		
 		int[][] grid2 = {{1,1,0,0,0},
 		        {1,1,0,0,0},
 		        {0,0,1,0,0},
 		        {0,0,0,1,1}};
 		System.out.println(numberOfIslands(grid2));
+		System.out.println(numberOfIslandsV2(grid2));
 	}
 }
